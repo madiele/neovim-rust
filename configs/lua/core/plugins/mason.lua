d9fc24d00fa3a0fb027fb82aaf9c01be6bc25a8b
Mattia Di Eleuterio <madiele92@gmail.com>
2023-03-26 22:09:55 +0200

    fix missing lsp

--- configs/lua/core/plugins/mason.lua
+++ configs/lua/core/plugins/mason.lua
@@ -5,22 +5,22 @@ local M = {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
   },
---  config = function()
---    -- install_root_dir = path.concat({ vim.fn.stdpath("data"), "mason" }),
---    require("mason").setup()
---
---    -- ensure tools (except LSPs) are installed
---    local mr = require("mason-registry")
---    for _, tool in ipairs(settings.tools) do
---      local p = mr.get_package(tool)
---      if not p:is_installed() then
---        p:install()
---      end
---    end
---
---    -- install LSPs
---    require("mason-lspconfig").setup({ ensure_installed = settings.lsp_servers })
---  end,
+  config = function()
+    -- install_root_dir = path.concat({ vim.fn.stdpath("data"), "mason" }),
+    require("mason").setup()
+
+    -- ensure tools (except LSPs) are installed
+    local mr = require("mason-registry")
+    for _, tool in ipairs(settings.tools) do
+      local p = mr.get_package(tool)
+      if not p:is_installed() then
+        p:install()
+      end
+    end
+
+    -- install LSPs
+    require("mason-lspconfig").setup({ ensure_installed = settings.lsp_servers })
+  end,
   build = function()
     -- install_root_dir = path.concat({ vim.fn.stdpath("data"), "mason" }),
     require("mason").setup()
